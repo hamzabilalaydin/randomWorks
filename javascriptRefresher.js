5880644a3ff7d43b872238583fc9794b4a306bf4
hamzabilalaydin <hamzaba61@gmail.com>
2025-01-25 22:26:00 +0300

    Single-Line Function Body (Implicit Return)

--- javascriptRefresher.js
+++ javascriptRefresher.js
@@ -3,14 +3,15 @@
 //     - Lunch
 //     - Dinner`);
 
-
 // const greet = name => `Hello, ${name}!`;
 // console.log(greet('John')); // Hello, John!
-
 
 // const sayHello = () => 'Hello, world!';
 // console.log(sayHello()); // Hello, world!
 
+// const add = (a, b) => a + b;
+// console.log(add(2, 3)); // 5
 
-const add = (a, b) => a + b;
-console.log(add(2, 3)); // 5
+//Single-Line Function Body (Implicit Return):
+// const multiply = (x, y) => x * y;
+// console.log(multiply(4, 5)); // 20
